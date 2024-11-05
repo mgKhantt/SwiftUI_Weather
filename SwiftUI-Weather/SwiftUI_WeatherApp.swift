81e7c3fde0531263e162625eafd16b7986e45039
mgKhantt <khantphonenaing156@gamil.com>
2024-11-05 14:09:02 +0630

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Khant Phone Naing  on 05/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
